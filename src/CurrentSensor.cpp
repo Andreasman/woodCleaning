b6bc20b911d98f718a40332cfbec20702b5e7dfb
Andreas Stäcker <andreas.staecker@t-online.de>
2020-12-06 18:49:59 +0100

    Refactoring CurrentSensor

--- src/CurrentSensor.cpp
+++ src/CurrentSensor.cpp
@@ -11,7 +11,6 @@ float CurrentSensor::getCurrent()
     float currentSensorValue = 0.0; // aktuell gemessener Sensorwert am Analogeingang
     float sumSensorValues = 0.0;    // Summe der Messwerte
     float averageSensorValue = 0.0; // geglätteter Sensorwert
-    float current = 0.0;            // gemessener geglätteter Strom in Ampere
 
     // Messwerte glätten
     for (int x = 0; x < 150; x++)
@@ -22,9 +21,10 @@ float CurrentSensor::getCurrent()
     }
     averageSensorValue = sumSensorValues / 150.0; // Glätten des Sensorwerts, Mittelwert bilden
 
-    /* ((averageSensorValue * (5.0 / 1024.0)) konvertiert die gelesene Spannung 0-5 Volt.
+    /* ((averageSensorValue * (5.0 / 1024.0)) konvertiert den gelesenen Stromwert in 0-5 Volt.
     2.5 Volt am Analogeingang entspricht 0 Ampere.
     0.185v(185mV) entsprechen 1 Ampere Stromaufnahme */
-    current = (2.5 - (averageSensorValue * (5.0 / 1024.0))) / 0.185;
-    return current;
+
+    // geglätte Stromaufnahme wird in Ampere ausgegeben
+    return (2.5 - (averageSensorValue * (5.0 / 1024.0))) / 0.185;
 }
