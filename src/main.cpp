8b00e877428ac08745dd120f4bf41fa47c9005cb
Andreas Stäcker <a.staecker@freibadhaenigsen.onmicrosoft.com>
2020-12-14 19:48:33 +0100

    Pin Belegung

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,7 @@
 #include <CurrentSensor.h>
 #include <Servo.h>
 
-// ### PIN BELEGUNG ###
-// Servos
-int servo1Pin = 10;      // Pin Servo 1
-const int servo2Pin = 9; // Pin Servo 2
+// ### PIN BELEGUNGEN ###
 // Taster
 const int s1Pin = 0;
 const int s2Pin = 0;
@@ -25,6 +22,39 @@ const int s5LedPin = 8;
 const int s6LedPin = 9;
 const int s7LedPin = 8;
 const int s8LedPin = 9;
+// Relais-PINs
+const int k1Machine1Pin=0;
+const int k2Machine2Pin=0;
+const int k3ServoPoperPin=0;
+// Stromsensoren Automatik-Steckdosen
+const int currentSensorPlug1Pin=0;
+const int currentSenosrPlug2Pin=0;
+const int currentSensorPlug3Pin=0;
+const int currentSenosrPlug4Pin=0;
+const int currentSensorPlug5Pin=0;
+const int currentSenosrPlug6Pin=0;
+const int currentSensorPlug7Pin=0;
+const int currentSenosrPlug8Pin=0;
+// Servo PINs
+const int servo1Pin = 0;
+const int servo2Pin = 0;
+const int servo3Pin = 0;
+const int servo4Pin = 0;
+const int servo5Pin = 0;
+const int servo6Pin = 0;
+const int servo7Pin = 0;
+const int servo8Pin = 0;
+// Stromsensoren Servoüberwachung
+const int currentSensorServo1Pin = 0;
+const int currentSensorServo2Pin = 0;
+const int currentSensorServo3Pin = 0;
+const int currentSensorServo4Pin = 0;
+const int currentSensorServo5Pin = 0;
+const int currentSensorServo6Pin = 0;
+const int currentSensorServo7Pin = 0;
+const int currentSensorServo8Pin = 0;
+
+
 
 // PROGRAMMVARIABLEN
 bool standbyMode = true;          // im Standby-Modus Servos Spannungslos schalten
